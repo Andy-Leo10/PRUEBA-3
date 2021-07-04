dc97c89aa95d211f9cd897e80ea2fbed248f0230
Andres Leonardo <alamoguizadoandresleonardo@gmail.com>
2021-07-04 12:29:05 -0500

    blabla

--- comentario only ejm.py
+++ comentario only ejm.py
@@ -2,12 +2,3 @@ import RPi.GPIO as GPIO
 import time
 import os
 
-GPIO.setmode(GPIO.BOARD)
-pin=3
-GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-while True:
-	apagar=GPIO.input(pin)
-	if apagar == False:
-		os.system("sudo shutdown now")
-GPIO.cleanup()
